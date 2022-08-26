df4138115beff4547678b577a9adb40d8787a447
David S. Martinez <56695799+davidmartinez13@users.noreply.github.com>
2022-08-26 14:43:45 +0200

    added comments and testing code for resnet backbone

--- modules/resnet.py
+++ modules/resnet.py
@@ -5,14 +5,14 @@ import torch.nn as nn
 class Bottleneck(nn.Module):
     expansion = 4
 
-    def __init__(self, inplanes, planes, stride=1, downsample=None, norm_layer=nn.BatchNorm2d):
+    def __init__(self, inchannels, channels, stride=1, downsample=None, norm_layer=nn.BatchNorm2d):
         super().__init__()
-        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
-        self.bn1 = norm_layer(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn2 = norm_layer(planes)
-        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
-        self.bn3 = norm_layer(planes * 4)
+        self.conv1 = nn.Conv2d(in_channels = inchannels, out_channels = channels, kernel_size=1, bias=False)
+        self.bn1 = norm_layer(num_features = channels)
+        self.conv2 = nn.Conv2d(in_channels = channels,out_channels = channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = norm_layer(num_features = channels)
+        self.conv3 = nn.Conv2d(in_channels = channels,out_channels = channels * 4, kernel_size=1, bias=False)
+        self.bn3 = norm_layer(num_features = channels * 4)
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
         self.stride = stride
@@ -45,42 +45,52 @@ class ResNet(nn.Module):
 
     def __init__(self, layers, block=Bottleneck, norm_layer=nn.BatchNorm2d):
         super().__init__()
-
+        # number of stages in resnet
         self.num_base_layers = len(layers)
         self.layers = nn.ModuleList()
         self.channels = []
         self.norm_layer = norm_layer
-        self.inplanes = 64
-
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
-        self.bn1 = norm_layer(64)
+        self.inchannels = 64
+        
+        # C1 Stage of Resnet
+        self.conv1 = nn.Conv2d(in_channels = 3, 
+                                out_channels = 64, 
+                                kernel_size=7, 
+                                stride=2, 
+                                padding=3, 
+                                bias=False)
+        self.bn1 = norm_layer(num_features = 64)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        # C2 Stage of Resnet
+        self._make_layer(block, channels = 64, blocks = layers[0])
+        # C3 Stage of Resnet
+        self._make_layer(block,channels = 128, blocks = layers[1], stride=2)
+        # C4 Stage of Resnet
+        self._make_layer(block,channels = 256, blocks = layers[2], stride=2)
+        # C5 Stage of Resnet
+        self._make_layer(block,channels = 512, blocks = layers[3], stride=2)
 
-        self._make_layer(block, 64, layers[0])
-        self._make_layer(block, 128, layers[1], stride=2)
-        self._make_layer(block, 256, layers[2], stride=2)
-        self._make_layer(block, 512, layers[3], stride=2)
-
+        # put Conv2d backbone layers into list
         self.backbone_modules = [m for m in self.modules() if isinstance(m, nn.Conv2d)]
 
-    def _make_layer(self, block, planes, blocks, stride=1):
+    def _make_layer(self, block, channels, blocks, stride=1):
         downsample = None
-
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(nn.Conv2d(self.inplanes, planes * block.expansion,
+        # Downsample if stride isn't default or if input channels aren't of proper size 
+        if stride != 1 or self.inchannels != channels * block.expansion:
+            downsample = nn.Sequential(nn.Conv2d(self.inchannels, channels * block.expansion,
                                                  kernel_size=1, stride=stride, bias=False),
-                                       self.norm_layer(planes * block.expansion))
-
-        layers = [block(self.inplanes, planes, stride, downsample, self.norm_layer)]
-        self.inplanes = planes * block.expansion
+                                       self.norm_layer(num_features = channels * block.expansion))
+        # Create layers
+        layers = [block(self.inchannels, channels, stride, downsample, self.norm_layer)]
+        self.inchannels = channels * block.expansion
 
         for i in range(1, blocks):
-            layers.append(block(self.inplanes, planes, norm_layer=self.norm_layer))
+            layers.append(block(self.inchannels, channels, norm_layer=self.norm_layer))
 
         layer = nn.Sequential(*layers)
 
-        self.channels.append(planes * block.expansion)
+        self.channels.append(channels * block.expansion)
         self.layers.append(layer)
 
     def forward(self, x):
@@ -102,3 +112,32 @@ class ResNet(nn.Module):
         state_dict = torch.load(path)
         self.load_state_dict(state_dict, strict=True)
         print(f'\nBackbone is initiated with {path}.\n')
+
+if __name__ == '__main__':
+    def construct_backbone(cfg_backbone=ResNet):
+        # resnet101 has 3, 4, 23, 3 blocks for each stage
+        backbone = cfg_backbone([3, 4, 23, 3])
+
+        # Add downsampling layers until we reach the number we need
+        selected_layers=[1, 2, 3]
+        num_layers = max(selected_layers) + 1
+
+        while len(backbone.layers) < num_layers:
+            backbone.add_layer()
+
+        return backbone
+
+    from torchvision import models
+    from torchsummary import summary
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # PyTorch v0.4.0
+    model = construct_backbone().to(device)
+    summary(model, (3, 550, 550))
+    input=torch.randn(1, 3, 550, 550)
+    backbone=construct_backbone()(input)
+
+    print('backbone output features :', len(backbone))
+    print('C2 output shape : ', backbone[0].shape)
+    print('C3 output shape : ', backbone[1].shape)
+    print('C4 output shape : ', backbone[2].shape)
+    print('C5 output shape : ', backbone[3].shape)
